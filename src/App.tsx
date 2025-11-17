9711df0aca830758fb9c4bbb8205e8c61dfa160d
robmolloy <robmolloy@hotmail.co.uk>
2025-11-17 12:29:03 +0000

    prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,123 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState, type ReactNode } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Marker = (p: { size: number; color: string; onClick: () => void }) => (
+  <div
+    onClick={p.onClick}
+    style={{
+      width: p.size,
+      height: p.size,
+      backgroundColor: p.color,
+      borderRadius: "50%",
+      border: "1px solid black",
+      cursor: "pointer",
+    }}
+  />
+);
+
+const PositionOverlayItem = (p: {
+  xPercent: number;
+  yPercent: number;
+  children: ReactNode;
+}) => (
+  <div
+    style={{
+      position: "absolute",
+      left: p.xPercent + "%",
+      top: p.yPercent + "%",
+      transform: "translate(-50%, -50%)",
+      pointerEvents: "auto",
+    }}
+  >
+    {p.children}
+  </div>
+);
+
+const InteractiveImage = (p: {
+  src: string;
+  onClick: (p: { xPercent: number; yPercent: number }) => void;
+  zoom: number;
+}) => {
+  const imgRef = useRef<HTMLImageElement>(null);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div style={{ overflow: "hidden", display: "inline-block" }}>
+      <img
+        src={p.src}
+        ref={imgRef}
+        onClick={(e) => {
+          if (!imgRef.current) return;
 
-export default App
+          const rect = imgRef.current.getBoundingClientRect();
+          const xPercent = ((e.clientX - rect.left) / rect.width) * 100;
+          const yPercent = ((e.clientY - rect.top) / rect.height) * 100;
+
+          p.onClick({ xPercent, yPercent });
+        }}
+        style={{
+          display: "block",
+          height: "auto",
+          cursor: "crosshair",
+          transform: `scale(${p.zoom})`,
+          transformOrigin: "top left",
+        }}
+        alt="Interactive"
+      />
+    </div>
+  );
+};
+
+const OverlayMarkerWrapper = (p: {
+  markers: { xPercent: number; yPercent: number }[];
+  children: ReactNode;
+  onMarkerClick: (x: { xPercent: number; yPercent: number }) => void;
+}) => {
+  return (
+    <div style={{ position: "relative", display: "inline-block" }}>
+      {p.children}
+      {p.markers.map((marker) => (
+        <PositionOverlayItem
+          key={`${marker.xPercent}-${marker.yPercent}`}
+          xPercent={marker.xPercent}
+          yPercent={marker.yPercent}
+        >
+          <Marker
+            size={10}
+            color={"white"}
+            onClick={() => {
+              p.onMarkerClick({
+                xPercent: marker.xPercent,
+                yPercent: marker.yPercent,
+              });
+            }}
+          />
+        </PositionOverlayItem>
+      ))}
+    </div>
+  );
+};
+
+const App = () => {
+  const [markers, setMarkers] = useState<
+    { xPercent: number; yPercent: number }[]
+  >([]);
+  const [zoom, setZoom] = useState(1);
+
+  return (
+    <div>
+      <div style={{ marginBottom: 10 }}>
+        <button onClick={() => setZoom((z) => z * 1.2)}>Zoom In</button>
+        <button onClick={() => setZoom((z) => z / 1.2)}>Zoom Out</button>
+      </div>
+
+      <OverlayMarkerWrapper markers={markers} onMarkerClick={() => {}}>
+        <InteractiveImage
+          src="https://upload.wikimedia.org/wikipedia/commons/8/85/Smiley.svg"
+          onClick={(coord) => setMarkers((markers) => [...markers, coord])}
+          zoom={zoom}
+        />
+      </OverlayMarkerWrapper>
+    </div>
+  );
+};
+
+export default App;
